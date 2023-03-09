b958cc2ffa8babab55f7acec05cb018cb708c04b
Pramod-1999 <127389766+Pramod-1999@users.noreply.github.com>
2023-03-09 15:40:08 +0530

    Add files via upload

---
+++ Naturalnumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Naturalnumbers
+ {
+    public static void main(String[] args) 
+    {
+	int i;
+	System.out.println("The First 10 Natural numbers are - ");
+	for(i=1;i<=10;i++)
+		System.out.println(i);
+    }
+}
