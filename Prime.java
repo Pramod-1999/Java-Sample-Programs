b8de06ea64dc2b31c7ea27ef07c62111955ea115
Pramod-1999 <127389766+Pramod-1999@users.noreply.github.com>
2023-03-30 14:54:40 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Prime
+ {
+    public static void main(String[] args) 
+    {
+	 int n,i,temp=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter the Number -");
+	n=sc.nextInt();
+	if(n==0 || n==1)
+	{
+		System.out.print(n+" neither Prime nor Composite.");
+	}
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			temp=1;
+			break;
+		}
+	}
+	if(temp==0)
+		System.out.print(n+" is Prime.");
+	else
+		System.out.print(n+" is not Prime.");
+    }
+}
