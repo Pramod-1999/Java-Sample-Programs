b0b80ca5bd5058d33c80febbe1c5230873f7277c
Pramod-1999 <127389766+Pramod-1999@users.noreply.github.com>
2023-03-30 14:40:42 +0530

    Add files via upload

---
+++ Bliteral.java
@@ -0,0 +1,11 @@
+class Bliteral
+ {
+    public static void main(String[] args) 
+    {
+        boolean b=true;
+        boolean c=false;
+      
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
