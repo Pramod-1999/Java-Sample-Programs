5c6d3642afc8577d57edfa6cd134983b9baa42b9
Pramod-1999 <127389766+Pramod-1999@users.noreply.github.com>
2023-03-30 14:48:56 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,11 @@
+public class Literals
+{
+    public static void main(String[] args) 
+    {
+        double a=101.01;
+        double b=88.75;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
