5cea5695692220c969ce648ea67eb16a3e80acb8
Pramod-1999 <127389766+Pramod-1999@users.noreply.github.com>
2023-03-30 14:42:22 +0530

    Add files via upload

---
+++ Cliterals.java
@@ -0,0 +1,10 @@
+class Cliterals
+{
+    public static void main(String[] args)
+    {
+        String s="Hello";
+        String s1="\n";
+        System.out.println(s);
+        System.out.println(s1);
+    }
+}
